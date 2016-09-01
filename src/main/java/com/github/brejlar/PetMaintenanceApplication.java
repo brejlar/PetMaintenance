974b95586f0615859ed6c189d0d5aedf74a2461a
Brennan Larson <brennan@SnekBook-Pro.local>
2016-08-31 17:07:27 -0700

    created project

---
+++ src/main/java/com/github/brejlar/PetMaintenanceApplication.java
@@ -0,0 +1,12 @@
+package com.github.brejlar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMaintenanceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/brejlar/PetMaintenanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.brejlar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetMaintenanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
